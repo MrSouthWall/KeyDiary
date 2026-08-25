ad04927605415ac3345a312ea0121f61cd897c1f
MrSouthWall <147359717+MrSouthWall@users.noreply.github.com>
2026-08-25 19:17:38 +0800

    Initial Commit

---
+++ KeyDiary/KeyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyDiaryApp.swift
+//  KeyDiary
+//
+//  Created by MrSouthWall on 2026/8/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
